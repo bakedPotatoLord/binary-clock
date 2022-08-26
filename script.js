13d1e4aceabcffeb7457db41dd2e871082573f7b
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2022-08-26 18:29:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let canvas = document.querySelector('canvas')
+
+var ctx= canvas.getContext('2d')
+
+
+let now;
+
+function circle(){
+	ctx.
+}
+
+
+canvas.width = 500;
+canvas.height=500;
+
+
+window.onload=()=>{
+	ctx.fillRect(10,10,50,50)
+	draw()
+}
+
+
+function draw(){
+	requestAnimationFrame(draw)
+	now = new Date()
+
+	now.get
+	
+}
