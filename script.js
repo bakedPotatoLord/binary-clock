1fc28261605b3c6f53f33baa334d3c30547c362b
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2022-08-27 01:22:59 +0000

    fixed all bugs

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 let c = document.querySelector('canvas')
+let btn = document.querySelector('button')
 
 var ctx= c.getContext('2d')
+ctx.textAlign = 'center'
+ctx.font = '40px Serif';
 
 let now;
 
-cw = 500
-ch=500
+let hintShown = false;
+
+cw=400
+ch=300
 
 c.width = cw;
 c.height=ch;
 
 let display =Array.from(Array(6),x=>x)
 let display2 =Array.from(Array(6),x=>x)
+
+function showHint(){
+	hintShown = !(hintShown)
+	if(hintShown){
+		btn.innerHTML = "Hide Hint"
+	}else{
+		btn.innerHTML = "Show Hint"
+	}
+}
 
 function circle(x,y,lit){
 	if(lit){
@@ -30,8 +44,8 @@ function clear(){
 
 function digitOf(num,digit){
 	let parsed = num.toString().split("")//.reverse()
-	if(parsed.length == 1) parsed.pop('0')
-	if(parsed[digit] == undefined) return '0'
+	if(parsed.length == 1) parsed.push('0')
+	//if(parsed[digit] == undefined) return '0'
 	return parsed[digit]
 }
 
@@ -50,10 +64,27 @@ function draw(){
 	display[2] = 	digitOf(now.getMinutes(),0)
 	display[3] = 	digitOf(now.getMinutes(),1)
 	//seconds
-	display[4] = 	digitOf(now.getSeconds(),0)
-	display[5] = 	digitOf(now.getSeconds(),1)
+	if(now.getSeconds() < 10){
+		display[5] = 	digitOf(now.getSeconds(),0)
+		display[4] = 	digitOf(now.getSeconds(),1)
+	}else{
+		display[4] = 	digitOf(now.getSeconds(),0)
+		display[5] = 	digitOf(now.getSeconds(),1)
+	}
+	
 
 	console.log(display)
+
+	if(hintShown){
+		ctx.fillStyle='black'
+		ctx.fillText(display[0],40,270)
+		ctx.fillText(display[1],100,270)
+		ctx.fillText(display[2],160,270)
+		ctx.fillText(display[3],220,270)
+		ctx.fillText(display[4],280,270)
+		ctx.fillText(display[5],340,270)
+	}
+
 	
 	//convert to binary
 	for(i in display){
@@ -63,34 +94,34 @@ function draw(){
 
 	//console.log(display2)
 
-	/*
+	
 	
 	//display 0
-	circle(40,400,display2[0][0])
-	circle(40,340,display2[0][1])
+	circle(40,220,display2[0][0])
+	circle(40,160,display2[0][1])
 	//display 1
-	circle(100,400,display2[1][0])
-	circle(100,340,display2[1][1])
-	circle(100,280,display2[1][2])
-	circle(100,220,display2[1][3])
+	circle(100,220,display2[1][0])
+	circle(100,160,display2[1][1])
+	circle(100,100,display2[1][2])
+	circle(100,40,display2[1][3])
 	//display 2
-	circle(160,400,display2[2][0])
-	circle(160,340,display2[2][1])
-	circle(160,280,display2[2][2])
+	circle(160,220,display2[2][0])
+	circle(160,160,display2[2][1])
+	circle(160,100,display2[2][2])
 	//display 3
-	circle(220,400,display2[3][0])
-	circle(220,340,display2[3][1])
-	circle(220,280,display2[3][2])
-	circle(220,220,display2[3][3])
+	circle(220,220,display2[3][0])
+	circle(220,160,display2[3][1])
+	circle(220,100,display2[3][2])
+	circle(220,40,display2[3][3])
 	//display 4
-	circle(280,400,display2[4][0])
-	circle(280,340,display2[4][1])
-	circle(280,280,display2[4][2])
+	circle(280,220,display2[4][0])
+	circle(280,160,display2[4][1])
+	circle(280,100,display2[4][2])
 	//display 5
-	circle(340,400,display2[5][0])
-	circle(340,340,display2[5][1])
-	circle(340,280,display2[5][2])
-	circle(340,220,display2[5][3])
+	circle(340,220,display2[5][0])
+	circle(340,160,display2[5][1])
+	circle(340,100,display2[5][2])
+	circle(340,40,display2[5][3])
 
-	*/
+	
 }
